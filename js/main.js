c4651d43c9f61150b5d826c4a4b672d4cff3995e
Nicolas Persivale <67163082+NPersivale@users.noreply.github.com>
2022-01-21 10:41:09 -0300

    added jQuery to the repository

--- js/main.js
+++ js/main.js
@@ -1,40 +1,53 @@
 // *******************************************************
+// DOM READY
+// *******************************************************
+
+$(() => {
+    console.log("El DOM esta listo");
+});
+
+
+// *******************************************************
 // CLASSES
 // *******************************************************
 
 class storeProduct {
-    constructor(id, name, price, image){
+    constructor(id, name, price, prodType, game, color, image){
         this.id = id;
         this.name = name;
         this.price = parseFloat(price);
+        this.prodType = prodType;
+        this.game = game;
+        this.color = color;
         this.image = image;
     }
 }
 
 class shoppingCartProd {
-    constructor(name, price){
+    constructor(name, price, prodType, game, color){
         this.name = name;
         this.price = price;
+        this.prodType = prodType;
+        this.game = game;
+        this.color = color;
     }
 }
 
 
 
 // *******************************************************
-// CODE
+// OBJECTS INSTANTIATING
 // *******************************************************
 
 const productsArray = [];
 
-// Store products - Object instances
-productsArray.push(new storeProduct(0, "iRacing Hoodie", 60, "../assets/Store/hoodie-iracing.jpg"));
-productsArray.push(new storeProduct(1, "iRacing T-Shirt", 30, "../assets/Store/tshirt-iracing.jpg"));
-productsArray.push(new storeProduct(2, "Escape From Tarkov T-Shirt", 30, "../assets/Store/tshirt-eft.jpg"));
-productsArray.push(new storeProduct(3, "Star Citizen Backpack", 70, "../assets/Store/SC-backpack.jpg"));
+// Store products
+productsArray.push(new storeProduct(0, "iRacing Hoodie", 60, "Apparel", "iRacing", "Gray", "../assets/Store/hoodie-iracing.jpg"));
+productsArray.push(new storeProduct(1, "iRacing T-Shirt", 30, "Apparel", "iRacing", "Gray", "../assets/Store/tshirt-iracing.jpg"));
+productsArray.push(new storeProduct(2, "Escape From Tarkov T-Shirt", 30, "Apparel", "EFT", "Black", "../assets/Store/tshirt-eft.jpg"));
+productsArray.push(new storeProduct(3, "Star Citizen Backpack", 70, "Luggage", "Star Citizen", "Black", "../assets/Store/SC-backpack.jpg"));
 
-
-let storeFeed = document.getElementById("storeFeed");
-createProduct(productsArray);
+$(`#storeFeed`).on('trigger', createProduct(productsArray)); //ASSOCIATED EVENT LISTENER
 
 
 
@@ -46,22 +59,13 @@ const shoppingCart = [];
 
 let storeBtn = document.getElementsByClassName("storeBtn");
 for (let  index = 0 ; index < storeBtn.length; index++) {
-    storeBtn[index].addEventListener('click' , addToCart) ; 
+    $(storeBtn[index]).on('click', addToCart);
 }
 console.log(shoppingCart); //TESTING PURPOSES ONLY
 
-
-let saveBtn = document.getElementById("saveBtn");
-saveBtn.addEventListener("click",saveCart);
-
-
-let recoverBtn = document.getElementById("recoverBtn");
-recoverBtn.addEventListener("click",recoverCart);
-
-
-let clearBtn = document.getElementById("clearBtn");
-clearBtn.addEventListener("click",resetStorage);
-
+$(`#saveBtn`).on('click', saveCart);
+$(`#recoverBtn`).on('click', recoverCart);
+$(`#clearBtn`).on('click', resetStorage);
 
 
 // *******************************************************
@@ -82,31 +86,32 @@ function recoverCart(){ //RECUPERA EL CARRITO DEL LOCALSTORAGE
     const storedProducts = JSON.parse(localStorage.getItem("shoppingCartLocal"));
     const recoveredProducts = [];
     for (const product of storedProducts){
-        recoveredProducts.push(new shoppingCartProd(product.name, product.price));
+        recoveredProducts.push(new shoppingCartProd(product.name, product.price, product.prodType, product.game, product.color));
     }
     console.log(recoveredProducts); //TESTING PURPOSES ONLY
 }
 
 function createProduct(productsArray) {
     for(products of productsArray){
-        let div = document.createElement("div");
-        div.className = "entry col-lg-5 col-sm-12";
-        div.innerHTML = `
-        <div class="storeItem">
-            <h2 class="h2Prod" >${products.name}</h2>
-            <img class="store__img" src=\"${products.image}">
-            <h3 class="priceProd" >Price: ${products.price}</h3>
-            <input class="btn storeBtn" id="${products.id}" type="submit" value="Add to Cart">
-        </div>
-        `;
-        storeFeed.appendChild(div);
+        $("#storeFeed").append(`
+        <div class="entry col-lg-5 col-sm-12">
+            <div class="storeItem">
+                <h2 class="prodH2" >${products.name}</h2>
+                <img class="store__img" src=\"${products.image}">
+                <h3 class="prodPrice" >Price: ${products.price}</h3>
+                <input class="prodType" value="${products.prodType}" type="hidden">
+                <input class="prodGame" value="${products.game}" type="hidden">
+                <input class="prodColor" value="${products.color}" type="hidden">
+                <input class="btn storeBtn" id="${products.id}" type="submit" value="Add to Cart">
+            </div>
+        </div>`);
     }
 }
 
 
 function addToCart(event){
     let targetId = event.target.id;
-    let addItem = document.getElementsByClassName("storeBtn")[targetId].parentElement.querySelectorAll(".priceProd, .h2Prod");
-    shoppingCart.push(new shoppingCartProd(addItem[0].innerHTML, addItem[1].innerHTML.replace(/[^0-9]/g,'')));
+    let addItem = document.getElementsByClassName("storeBtn")[targetId].parentElement.querySelectorAll(".prodPrice, .prodH2, .prodType, .prodGame, .prodColor");
+    shoppingCart.push(new shoppingCartProd(addItem[0].innerHTML, addItem[1].innerHTML.replace(/[^0-9]/g,''), addItem[2].value, addItem[3].value, addItem[4].value));
     console.log(shoppingCart); //TESTING PURPOSES ONLY
 }
